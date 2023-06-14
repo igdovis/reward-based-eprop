9f0277fec33d6c2a34e1675472d45e016268a0d2
igdovis <42443095+igdovis@users.noreply.github.com>
2023-06-14 14:36:24 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+from utils import train_reward_AC
+from RSNN_ALIF import RSNN_ALIF
+from LIF import LIF
+
+lr_layer_norm = (0.05, 0.05, 1.0)
+# Network model parameters
+n_rec = 128
+threshold = 1
+tau_mem = 20e-3
+tau_out = 20e-3
+bias_out = 0.0
+gamma = 0.3
+w_init_gain = (0.5, 0.1, 0.5)
+n_in = 8
+n_out = 2
+seq_len = 50
+rho = 0.985
+beta = 0.1
+
+alif = RSNN_ALIF(
+           n_in,
+           n_rec,
+           n_out,
+           seq_len,
+           threshold,
+           tau_mem,
+           tau_out,
+           bias_out,
+           gamma,
+           dt=1e-3,
+           w_init_gain=w_init_gain,
+           rho=rho,
+           beta=beta,
+           lr_layer=lr_layer_norm,
+           device="cuda"
+)
+device = torch.device("cuda")
+alif.to(device)
+train_reward_AC(alif, "ALIF")
+
+"""
+lif = LIF(n_in,
+               n_rec,
+               n_out,
+               seq_len,
+               threshold,
+               tau_mem,
+               tau_out,
+               bias_out,
+               gamma,
+               dt=1e-3,
+               model='LIF',
+               classif=True,
+               w_init_gain=w_init_gain,
+               lr_layer=lr_layer_norm,
+               t_crop=0,
+               visualize=False,
+               visualize_light=False,
+               device="cuda"
+           )
+lif = lif.to(device)
+
+train_reward_AC(lif, "LIF")"""
